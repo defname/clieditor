54d0855515ed05b9b38f682381da14f374af4d7d
defname <bodo.akdeniz@gmail.com>
2025-09-28 13:16:18 +0200

    fix: rename logging functions to not start with an underscore

--- src/utils/logging.h
+++ src/utils/logging.h
@@ -17,24 +17,24 @@ typedef enum {
 
 void logger(LogLevel level, const char* format, ...);
 
-void _logDebug(const char* format, ...);
-void _logInfo(const char* format, ...);
-void _logWarn(const char* format, ...);
-void _logError(const char* format, ...);
-void _logFatal(const char* format, ...);
+void logDebugImpl(const char* format, ...);
+void logInfoImpl(const char* format, ...);
+void logWarnImpl(const char* format, ...);
+void logErrorImpl(const char* format, ...);
+void logFatalImpl(const char* format, ...);
 
 #ifdef DEBUG
-#define logDebug(fmt, ...) _logDebug("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-#define logInfo(fmt, ...) _logInfo("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-#define logWarn(fmt, ...) _logWarn("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-#define logError(fmt, ...) _logError("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-#define logFatal(fmt, ...) _logFatal("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define logDebug(fmt, ...) logDebugImpl("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define logInfo(fmt, ...) logInfoImpl("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define logWarn(fmt, ...) logWarnImpl("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define logError(fmt, ...) logErrorImpl("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define logFatal(fmt, ...) logFatalImpl("\n -- [%s:%d] %s\n -- " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #else
-#define logDebug(fmt, ...) _logDebug(fmt, ##__VA_ARGS__)
-#define logInfo(fmt, ...) _logInfo(fmt, ##__VA_ARGS__)
-#define logWarn(fmt, ...) _logWarn(fmt, ##__VA_ARGS__)
-#define logError(fmt, ...) _logError(fmt, ##__VA_ARGS__)
-#define logFatal(fmt, ...) _logFatal(fmt, ##__VA_ARGS__)
+#define logDebug(fmt, ...) logDebugImpl(fmt, ##__VA_ARGS__)
+#define logInfo(fmt, ...) logInfoImpl(fmt, ##__VA_ARGS__)
+#define logWarn(fmt, ...) logWarnImpl(fmt, ##__VA_ARGS__)
+#define logError(fmt, ...) logErrorImpl(fmt, ##__VA_ARGS__)
+#define logFatal(fmt, ...) logFatalImpl(fmt, ##__VA_ARGS__)
 #endif
 
 #endif
