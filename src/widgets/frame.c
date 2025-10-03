7ee2170456e05cad1de736ec97ff66f7b0cd3d42
defname <bodo.akdeniz@gmail.com>
2025-10-03 08:40:36 +0200

    feat: change border style with Frame_SetBorderStyle() or Frame_SetBoxDrawingCharacters()

--- src/widgets/frame.c
+++ src/widgets/frame.c
@@ -1,51 +1,65 @@
 #include "frame.h"
 
+#include <string.h>
+
 #include "common/logging.h"
 #include "common/utf8string.h"
 
+// 0:┌ 1:─ 2:┒ 3:│ 4:┕ 5:┛ 6:┃ 7:━
+static const char *box_drawing_chars[] = {
+    "┌─┐│└┘│─",
+    "┏━┓┃┗┛┃━",
+    "╔═╗║╚╝║═"
+};
+
 typedef struct {
     Widget *container;
+    UTF8String charset;
 } FrameData;
 
 static void frame_destroy(Widget *self) {
     FrameData *data = self->data;
     if (data) {
+        UTF8String_Deinit(&data->charset);
         free(data);
         self->data = NULL;
     }
 }
 
 static void frame_on_resize(Widget *self, int new_parent_width, int new_parent_height) {
+    (void)self;
+    (void)new_parent_width;
+    (void)new_parent_height;
 }
 
 static void frame_draw(const Widget *self, Canvas *canvas) {
+    FrameData *data = self->data;
+    UTF8Char *border_chars = data->charset.chars;
+
     // Draw a border
-    UTF8String border_chars;
-    UTF8String_Init(&border_chars);
-    UTF8String_FromStr(&border_chars, "┌─┐│└┘", 6); // 0:┌, 1:─, 2:┐, 3:│, 4:└, 5:┘
+
     // top border
-    Canvas_PutChar(canvas, border_chars.chars[0]);
+    Canvas_PutChar(canvas, border_chars[0]);
     for (int i=0; i<self->width-2; i++) {
-        Canvas_PutChar(canvas, border_chars.chars[1]); 
+        Canvas_PutChar(canvas, border_chars[1]); 
     }
-    Canvas_PutChar(canvas, border_chars.chars[2]);
+    Canvas_PutChar(canvas, border_chars[2]);
+
     // sides
-    for (int i=0; i<self->height-2; i++) {
-        Canvas_PutChar(canvas, border_chars.chars[3]);
-        for (int j=0; j<self->width-2; j++) {
-            Canvas_PutChar(canvas, utf8_space);
-        }
-        Canvas_PutChar(canvas, border_chars.chars[3]);
+    for (int i=1; i<self->height-1; i++) {
+        Canvas_MoveCursor(canvas, 0, i);
+        Canvas_PutChar(canvas, border_chars[3]);
+        Canvas_MoveCursor(canvas, self->width-1, i);
+        Canvas_PutChar(canvas, border_chars[6]);
     }
 
     // bottom border
-    Canvas_PutChar(canvas, border_chars.chars[4]);
-    for (int i=0; i<self->width-2; i++) {
-        Canvas_PutChar(canvas, border_chars.chars[1]);
+    Canvas_MoveCursor(canvas, 0, self->height-1);
+    Canvas_PutChar(canvas, border_chars[4]);
+    for (int i=1; i<self->width-1; i++) {
+        Canvas_PutChar(canvas, border_chars[7]);
     }
-    Canvas_PutChar(canvas, border_chars.chars[5]);
-
-    UTF8String_Deinit(&border_chars);
+    Canvas_PutChar(canvas, border_chars[5]);
 }
 
 static void container_on_resize(Widget *container, int parent_w, int parent_h) {
@@ -69,7 +83,49 @@ Widget *Frame_Create(Widget *parent) {
     if (!data) {
         logFatal("Cannot allocate memory for FrameData.");
     }
+
+    UTF8String_Init(&data->charset);
+
+    self->style.bg = 19;
+
     data->container = Widget_Create(self, &container_ops);
+    data->container->x = 1;
+    data->container->y = 1;
     self->data = data;
+
+    Frame_SetBorderStyle(self, 0);  // needs self->data to be set before
     return self;
+}
+
+void Frame_AddChild(Widget *self, Widget *child) {
+    FrameData *data = self->data;
+    if (!data) {
+        logFatal("Frame has no data.");
+    }
+    Widget_AddChild(data->container, child);
+}
+
+void Frame_SetBorderStyle(Widget *self, unsigned char style) {
+    if (style > (sizeof(box_drawing_chars) / sizeof(box_drawing_chars[0]))) {
+        logWarn("style out of bounds. Border style not set.");
+        return;
+    }
+    if (!self || !self->data) {
+        logError("Invalid widget.");
+        return;
+    }
+    Frame_SetBoxDrawingCharacters(self, box_drawing_chars[style]);
+}
+
+void Frame_SetBoxDrawingCharacters(Widget *self, const char *chars) {
+    if (!self || !self->data) {
+        logError("Invalid widget.");
+        return;
+    }
+    FrameData *data = self->data;
+    UTF8String_FromStr(&data->charset, chars, strlen(chars));
+    if (data->charset.length != 8) {
+        logError("Invalid charset length.");
+        Frame_SetBorderStyle(self, 0);
+    }
 }
--- src/widgets/frame.h
+++ src/widgets/frame.h
@@ -5,4 +5,13 @@
 
 Widget *Frame_Create(Widget *parent);
 
+// This might be not usedful, since Widget_AddChild() is automatically called
+// by Widget_Create(parent).
+// Just make sure to use frame->data->container as parent when adding
+// children to a frame
+void Frame_AddChild(Widget *self, Widget *child);
+
+void Frame_SetBorderStyle(Widget *self, unsigned char style);
+void Frame_SetBoxDrawingCharacters(Widget *self, const char *chars);
+
 #endif
