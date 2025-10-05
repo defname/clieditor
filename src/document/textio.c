83983adce60461c63aa178f8abe5a583b56b3314
defname <bodo.akdeniz@gmail.com>
2025-10-05 08:13:07 +0200

    hotfix: correct line order in TB_LoadFromFile()

--- src/document/textio.c
+++ src/document/textio.c
@@ -4,12 +4,14 @@
 
 void TB_LoadFromFile(TextBuffer *tb, File *file) {
     TB_ReInit(tb);
+    Line *first_line = tb->current_line;
     Line *current = tb->current_line;
     UTF8String *line;
     while ((line = File_ReadLine(file)) != NULL) {
         UTF8String_Copy(&current->text, line);
         UTF8String_Destroy(line);
         current = TB_InsertLineAfter(tb);
+        tb->current_line = current;
     }
     // current is now a last empty line which was not in the document
     // so delete it
@@ -17,6 +19,7 @@ void TB_LoadFromFile(TextBuffer *tb, File *file) {
         Line_Delete(current);
         tb->line_count--;
     }
+    tb->current_line = first_line;
 }
 
 void TB_SaveToFile(const TextBuffer *tb, File *file) {
