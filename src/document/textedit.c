1f75b5d0ee500367c6308cb1a03b5d651825e1a6
defname <bodo.akdeniz@gmail.com>
2025-10-07 13:40:00 +0200

    Refactor textedit.h/c. Introduce TextEdit to manipulate a TextBuffer with respect to TextLayout. Add functions for cursor movement.

--- src/document/textedit.c
+++ src/document/textedit.c
@@ -1,74 +1,118 @@
 #include "textedit.h"
+#include "common/logging.h"
 
-#include "line.h"
-
-Line *TB_InsertLineAfter(TextBuffer *tb) {
-    Line *new_line = Line_Create();
-    tb->line_count++;
-    Line_InsertAfter(tb->current_line, new_line);
-
-    return new_line;
+void TextEdit_Init(TextEdit *te, TextBuffer *tb, TextLayout *tl) {
+    if (!te || !tb || !tl || tb != tl->tb) {
+        logError("Invalid arguments to initialize TextEdit().");
+        return;
+    }
+    te->tb = tb;
+    te->tl == tl;
+}
+void TextEdit_Deinit(TextEdit *te) {
+    te->tb = NULL;
+    te->tl = NULL;
 }
 
-void TB_DeleteCurrentLine(TextBuffer *tb) {
-    Line *former_current = tb->current_line;
-    if (former_current->prev) {
-        tb->current_line = former_current->prev;
+// --- Cursor movement ---
+void TextEdit_MoveLeft(TextEdit *te) {
+    TextBuffer *tb = te->tb;
+    TextBuffer_MergeGap(tb);
+    
+    if (tb->gap.position > 0) {
+        tb->gap.position--;
+        return;
     }
-    else if (former_current->next) {
-        tb->current_line = former_current->next;
+    if (tb->current_line->prev) {
+        tb->current_line = tb->current_line->prev;
+        tb->gap.position = tb->current_line->text.length;
     }
     else {
-        UTF8String_Shorten(&tb->current_line->text, 0);
+        tb->gap.position = 0;
+    }
+}
+
+void TextEdit_MoveRight(TextEdit *te) {
+    TextBuffer *tb = te->tb;
+    TextBuffer_MergeGap(tb);
+
+    if (tb->gap.position < tb->current_line->text.length) {
+        tb->gap.position++;
         return;
     }
-    Line_Delete(former_current);
-    tb->line_count--;
+    else if (tb->current_line->next) {
+        tb->current_line = tb->current_line->next;
+        tb->gap.position = 0;
+    }
+    else {
+        tb->gap.position = tb->current_line->text.length;
+    }
 }
 
-void TB_InsertChar(TextBuffer *tb, UTF8Char ch) {
-    UTF8String_AddChar(&tb->gap.text, ch);
-}
+void TextEdit_MoveUp(TextEdit *te) {
+    TextBuffer *tb = te->tb;
+    TextLayout *tl = te->tl;
+    TextBuffer_MergeGap(tb);
 
-void TB_Backspace(TextBuffer *tb) {
-    if (tb->gap.position == 0 && tb->gap.text.length == 0) {
-        TB_MergeGap(tb);
-        if (tb->current_line->prev == NULL) {
+    int cursor_x = TextLayout_GetCursorX(tl);
+    int cursor_y = TextLayout_GetCurostY(tl);
+
+    if (cursor_y == 0) {
+        // try to scroll up 
+        if (!TextLayout_ScrollUp(tl)) {
+            // jump to the beginning of the line if scrolling was not possible (begin of document reached)
+            tb->gap.position = 0;
             return;
         }
-        int cursor_pos = tb->current_line->prev->text.length;
-        UTF8String_Concat(&tb->current_line->prev->text, &tb->current_line->text);
-        TB_DeleteCurrentLine(tb);
-        tb->gap.position = cursor_pos;
-        return;
+        cursor_y = 1;
     }
-    if (tb->gap.text.length == 0) {
-        if (tb->gap.position <= tb->gap.overlap) {
-            TB_MergeGap(tb);
-            TB_Backspace(tb);
+    VisualLine *line_above = TextLayout_GetVisualLine(tl, cursor_y - 1);
+    tb->current_line = line_above->src;
+    tb->gap.position = line_above->offset + cursor_x;
+    
+    // fix position out of bounds
+    if (tb->gap.position > tb->current_line->text.length) {
+        tb->gap.position = tb->current_line->text.length;
+    }
+}
+
+void TextEdit_MoveDown(TextEdit *te) {
+    TextBuffer *tb = te->tb;
+    TextLayout *tl = te->tl;
+    TextBuffer_MergeGap(tb);
+
+    int cursor_x = TextLayout_GetCursorX(tl);
+    int cursor_y = TextLayout_GetCurostY(tl);
+
+    if (cursor_y == tl->height-1) {
+        // try to scroll down 
+        if (!TextLayout_ScrollDown(tl)) {
+            // jump to the end of the line if scrolling was not possible (end of document reached)
+            tb->gap.position = tb->current_line->text.length;
             return;
         }
-        tb->gap.overlap++;
+        cursor_y--;
+    }
+    VisualLine *line_below = TextLayout_GetVisualLine(tl, cursor_y - 1);
+    if (!line_below) {
+        tb->gap.position = tb->current_line->text.length;
         return;
     }
-    // gap length > 0
-    tb->gap.text.length--;
+    tb->current_line = line_below->src;
+    tb->gap.position = line_below->offset + cursor_x;
+
+    // fix position out of bounds
+    if (tb->gap.position > tb->current_line->text.length) {
+        tb->gap.position = tb->current_line->text.length;
+    }
 }
 
-void TB_Enter(TextBuffer *tb) {
-    TB_MergeGap(tb);
-    UTF8String before, after;
-    UTF8String_Init(&before);
-    UTF8String_Init(&after);
-    UTF8String_Split(&tb->current_line->text, &before, &after, tb->gap.position);
-    tb->current_line->text.length = tb->gap.position;
-    TB_InsertLineAfter(tb);
-    Line *next = tb->current_line->next;
-    UTF8String_Copy(&next->text, &after);
-    tb->current_line = next;
-    tb->gap.position = 0;
+// --- Editing ---
+void TextEdit_InsertChar(TextEdit *te, char c);
+void TextEdit_DeleteChar(TextEdit *te);      // delete at cursor
+void TextEdit_Backspace(TextEdit *te);       // delete befoTextBuffer_MergeGap(te->tb);re cursor
+void TextEdit_Newline(TextEdit *te);
 
+// --- Optional convenience ---
+void TextEdit_InsertString(TextEdit *te, const char *text);
 
-    UTF8String_Deinit(&before);
-    UTF8String_Deinit(&after);
-}
--- src/document/textedit.h
+++ src/document/textedit.h
@@ -2,13 +2,29 @@
 #define TEXTEDIT_H
 
 #include "textbuffer.h"
+#include "textlayout.h"
 
-Line *TB_InsertLineAfter(TextBuffer *tb);
+typedef struct _TextEdit {
+    TextBuffer *tb;
+    TextLayout *tl;
+} TextEdit;
 
-void TB_InsertChar(TextBuffer *tb, UTF8Char ch);
+void TextEdit_Init(TextEdit *te, TextBuffer *tb, TextLayout *tl);
+void TextEdit_Deinit(TextEdit *te);
 
-void TB_Backspace(TextBuffer *tb);
-void TB_Delete(TextBuffer *tb);
-void TB_Enter(TextBuffer *tb);
+// --- Cursor movement ---
+void TextEdit_MoveLeft(TextEdit *te);
+void TextEdit_MoveRight(TextEdit *te);
+void TextEdit_MoveUp(TextEdit *te);
+void TextEdit_MoveDown(TextEdit *te);
+
+// --- Editing ---
+void TextEdit_InsertChar(TextEdit *te, char c);
+void TextEdit_DeleteChar(TextEdit *te);      // delete at cursor
+void TextEdit_Backspace(TextEdit *te);       // delete before cursor
+void TextEdit_Newline(TextEdit *te);
+
+// --- Optional convenience ---
+void TextEdit_InsertString(TextEdit *te, const char *text);
 
 #endif
