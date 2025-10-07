4e3e4908d32608c44d585f87a6f285b88e75791c
defname <bodo.akdeniz@gmail.com>
2025-10-07 12:03:34 +0200

    add documentation

--- src/document/textlayout.h
+++ src/document/textlayout.h
@@ -1,3 +1,7 @@
+/**
+ * This module is responsible for calculating which lines are visible, where they are wrapped
+ * and where the cursor actually is on screen.
+ */
 #ifndef TEXTLAYOUT_H
 #define TEXTLAYOUT_H
 
@@ -5,7 +9,9 @@
 
 #include <stdbool.h>
 
-
+/**
+ * @brief Represents a visual line on screen.
+ */
 typedef struct _VisualLine {
     Line *src;
     int offset;     //< where does the virtual line start. offset in characters
@@ -15,15 +21,22 @@ void VisualLine_Init(VisualLine *vl, Line *src, int offset);
 void VisualLine_Deinit(VisualLine *vl);
 
 
+/**
+ * @brief Hold width and height and a cache of calculated VisualLines.
+ */
 typedef struct _TextLayout {
-    const TextBuffer *tb;
+    const TextBuffer *tb;  //< const since this module does only calculates information and doesn't change anything
+
+    // This properties can and should be manipulated by the user
     int width;
     int height;
     VisualLine first_line;
 
+    // This is created automatically
     VisualLine *cache;
     size_t cache_capacity;
 
+    // Should be set if tb is manipulated
     bool dirty;
 } TextLayout;
 
