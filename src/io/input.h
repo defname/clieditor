b4441af84ede02b7e867a19cbed9033bec4044a8
defname <bodo.akdeniz@gmail.com>
2025-10-12 14:48:01 +0200

    update documentation in input.h

--- src/io/input.h
+++ src/io/input.h
@@ -13,24 +13,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+
 /**
  * input.h
- * Read and buffer input to recognize ESC sequences.
- * It's important to empty the buffer after Input_Read() by calling
- * Input_GetChar() repeatedly!
- * 
- * Usage:
- * 
- * EscapeSequence e = Input_Read()  // read from terminal
- * 
- * while ((UTF8Char ch = Input_GetChar()).length > 0) {
- *     handleInputChar(ch);
- * }
- * 
- * Expected behavier:
- * Inpu_Read() returns the escape sequence that was read or ESC_NONE. If ESC_NONE is returned
- * all symbols that were part of a started sequence are discarded.
- * ESC will never occure in the output of Input_GetChar()
+ * The stream of multi byte characters and escape sequences is recognized and
+ * packed into InputEvent structures.
+ * The only needed function is
+ *   Input_Read()
+ * which should be called at the beginning of every iteration of the main loop.
+ * The returned struct contains a keycode or a character (not both) together
+ * with optional modification flags.
+ * Note that the KEY_MOD_SHIFT will only be set together with a keycode. Characters
+ * or symbols will just be in uppercase.
+ * (like Shift+ArrowLeft will result in key=KEY_LEFT and mod & KEY_MOD_SHIFT,
+ * but Shift+"a" is just "A")
  */
 #ifndef INPUT_H
 #define INPUT_H
@@ -67,8 +64,8 @@ typedef struct _InputEvent {
 
 bool InputEvent_IsValid(const InputEvent *ev);
 
-void Input_Init();
-void Input_Deinit();
+void Input_Init();      //< deprecated
+void Input_Deinit();    //< deprecated
 InputEvent Input_Read();
 
 
