5f544e9059a605b21aab08a5b13eef7fde63949e
defname <bodo.akdeniz@gmail.com>
2025-10-20 22:04:17 +0200

    add additional tests

--- tests/test_tableiterator.c
+++ tests/test_tableiterator.c
@@ -37,11 +37,52 @@ void test_standard(void) {
 
     fill_table(keys, values, count);
     fill_table(keys, values, 0);
-    
+    fill_table(keys, values, 1);
 }
+
+void test_iterating_twice(void) {
+    Table *table = Table_Create();
+    Table_Set(table, "key", "value", NULL);
+    Table_Set(table, "key2", "value2", NULL);
+    Table_Set(table, "key3", "value3", NULL);
+
+    TableIterator it = TableIterator_Begin(table);
+    size_t count = 0;
+    while (TableIterator_Next(&it)) {
+        count++;
+    }
+    TEST_CHECK(count == 3);
+
+    // run second time without reinitializing
+    count = 0;
+    while (TableIterator_Next(&it)) {
+        count++;
+    }
+    TEST_CHECK(count == 0);
+
+    // run again with reinitializing
+    it = TableIterator_Begin(table);
+    count = 0;
+    while (TableIterator_Next(&it)) {
+        count++;
+    }
+    TEST_CHECK(count == 3);
+
+    Table_Destroy(table);
+}
+
+void test_edgecases(void) {
+    TableIterator it = TableIterator_Begin(NULL);
+    TEST_CHECK(!TableIterator_Next(&it));
+
+    TEST_CHECK(!TableIterator_Next(NULL));
+}
+
 
 TEST_LIST = {
     { "TableIterator: Standard", test_standard },
+    { "TableIterator: Iterating twice", test_iterating_twice },
+    { "TableIterator: Edgecases", test_edgecases },
     { NULL, NULL }
 };
 
