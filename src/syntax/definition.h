90c1acd3c2b6f4cbd04fbf8316524f9fa8ec0286
defname <bodo.akdeniz@gmail.com>
2025-10-21 19:47:02 +0200

    add functiond to create SyntaxDefinition_FromTable() with tests. More tests needed

--- src/syntax/definition.h
+++ src/syntax/definition.h
@@ -6,11 +6,36 @@
 #include "common/table.h"
 #include "common/string.h"
 
+
+typedef enum {
+    SYNTAXDEFINITION_NO_ERROR,
+
+    SYNTAXDEFINITION_BLOCK_NO_START_REGEX,
+    SYNTAXDEFINITION_REGEX_ERROR_START,
+    SYNTAXDEFINITION_REGEX_ERROR_END,
+    SYNTAXDEFINITION_NO_META,
+    SYNTAXDEFINITION_BLOCK_NOT_A_SECTION,
+    SYNTAXDEFINITION_BLOCK_NAME_EMPTY,
+    SYNTAXDEFINITION_NO_ROOT_BLOCK,
+    SYNTAXDEFINITION_BLOCK_DOES_NOT_EXIST
+} SyntaxDefinitionErrorCode;
+
+/**
+ * @brief Holds error information. 
+ */
+typedef struct _SyntaxDefinitionError {
+    SyntaxDefinitionErrorCode code;
+    String message;
+} SyntaxDefinitionError;
+
+void SyntaxDefinitionError_Deinit(SyntaxDefinitionError *error);
+
+
 /**
  * @brief Holds the definition of a syntax block.
  */
 typedef struct _SyntaxBlockDef {
-    char name[12];      //< name of the block definition. for easier debugging
+    char *name;      //< name of the block definition. for easier debugging
     regex_t start;      //< compiled regex to determine the begin of the block
     regex_t end;        //< compiled regex to determine the end of the block (optional)
     
@@ -24,6 +49,7 @@ typedef struct _SyntaxBlockDef {
 
 SyntaxBlockDef *SyntaxBlockDef_Create();
 void SyntaxBlockDef_Destroy(SyntaxBlockDef *block);
+SyntaxBlockDef *SyntaxBlockDef_FromTable(const char *name, const Table *table, SyntaxDefinitionError *error);
 
 
 /**
@@ -33,7 +59,9 @@ typedef struct _SyntaxDefinition {
     char *name;                 //< name of the filetype (e.g. INI)
     SyntaxBlockDef *root;       //< root block definition
     SyntaxBlockDef **blocks;    //< list of all blocks
+    size_t blocks_count;         //< number of blocks
 } SyntaxDefinition;
+
 
 /**
  * @brief Constructs a SyntaxDefinition from a typed Table.
@@ -43,8 +71,7 @@ typedef struct _SyntaxDefinition {
  * @returns
  * A SyntaxDefinition or NULL if the definition in table is not valid.
  */
-SyntaxDefinition *SyntaxDefinition_FromTable(const Table *table);
+SyntaxDefinition *SyntaxDefinition_FromTable(const Table *table, SyntaxDefinitionError *error);
 void SyntaxDefinition_Destroy(SyntaxDefinition *def);
-
 
 #endif
