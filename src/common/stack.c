0b223a6b149868f21366ac1ad7c5963a3f180624
defname <bodo.akdeniz@gmail.com>
2025-10-25 08:56:40 +0200

    fix resizing in Stack_CopyTo()

--- src/common/stack.c
+++ src/common/stack.c
@@ -84,13 +84,13 @@ void Stack_CopyTo(Stack *dst, const Stack *src) {
         return;
     }
     if (src->size == 0) {
-        // ealry exit if src is empty
+        // early exit if src is empty
         Stack_Clear(dst);
         return;
     }
     if (src->size > dst->capacity) {
         // resize dst to a multiple of STACK_INITIAL_SIZE
-        size_t new_cap = ((src->size % STACK_INITIAL_CAPACITY) + 1) * STACK_INITIAL_CAPACITY;
+        size_t new_cap = ((src->size + STACK_INITIAL_CAPACITY - 1) / STACK_INITIAL_CAPACITY) * STACK_INITIAL_CAPACITY;
         resize_capacity(dst, new_cap);
     }
     // copy items
