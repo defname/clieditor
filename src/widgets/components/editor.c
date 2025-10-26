c32523100f6343b099e497d686ba673e297407a6
defname <bodo.akdeniz@gmail.com>
2025-10-26 08:25:39 +0100

    fix Table_Get() call in draw_visual_line() to use the correct key

--- src/widgets/components/editor.c
+++ src/widgets/components/editor.c
@@ -74,7 +74,7 @@ static int draw_visual_line(Editor *editor, Canvas *canvas, int y, int y_offset)
     }
 
     SyntaxHighlighting *sh = editor->sh_binding.sh;
-    SyntaxHighlightingString *shs = Table_Get(sh->strings, line->src);
+    SyntaxHighlightingString *shs = Table_Get(sh->strings, &line->src->text);
 
     // Move cursor to the start position
     Canvas_MoveCursor(canvas, 0, y + y_offset);
